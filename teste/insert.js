3e11c1dabb0c5b37d1eac5b1631f115e01a280b7
Erislândio Soares <erislandio.soares@acct.global>
2019-07-06 15:11:31 -0300

    add create index table pefis

---
+++ teste/insert.js
@@ -0,0 +1,18 @@
+const db = require("../config/db");
+
+const novoPerfil = {
+  nome: "cadastrador",
+  rotulo: "cadastrador"
+};
+
+db("perfis")
+  .insert(novoPerfil)
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err.sqlMessage);
+  })
+  .finally(() => {
+    db.destroy();
+  });
